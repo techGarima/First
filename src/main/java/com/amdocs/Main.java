b8ee3d9b8d8ace37f0473fcc57d5d6859b7f96a8
Garima <techlearner.tl@gmail.com>
2023-01-08 00:11:44 +0530

    Hello World

---
+++ src/main/java/com/amdocs/Main.java
@@ -0,0 +1,14 @@
+package com.amdocs;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Enter a number::");
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        System.out.println("the number is ::"+x);
+
+    }
+}
